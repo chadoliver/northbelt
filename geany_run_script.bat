56be3bc13ba802469c8d4cfe8da01e98bb6f34be
chad oliver <accounts@chadoliver.co.nz>
2013-08-03 00:22:45 -0400

    current state of c9 workspace

--- geany_run_script.bat
+++
@@ -1,6 +0,0 @@
-"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe"  -P "D:\ChibiOS\projects\serialLink\build\ch.hex" -V -Rst && exit
-
-pause
-del "%0"
-
-pause
